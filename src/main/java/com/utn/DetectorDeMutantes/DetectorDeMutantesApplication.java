571eb07c0a180ffc694186bf9b5dfee1baf2f221
Paula <110248761+Paula-Bomprezzi@users.noreply.github.com>
2025-11-19 03:24:04 -0300

    initial commit

---
+++ src/main/java/com/utn/DetectorDeMutantes/DetectorDeMutantesApplication.java
@@ -0,0 +1,13 @@
+package com.utn.DetectorDeMutantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorDeMutantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectorDeMutantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/DetectorDeMutantes/DetectorDeMutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.DetectorDeMutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectorDeMutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
