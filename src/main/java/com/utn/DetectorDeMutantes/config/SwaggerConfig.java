3fc34cfc2865d01403b27b5b7f6385159c3dfb34
Paula <paulabomp@gmail.com>
2025-11-22 22:22:43 -0300

    Configurado Swagger con link de render.... FIN??

--- src/main/java/com/utn/DetectorDeMutantes/config/SwaggerConfig.java
+++ src/main/java/com/utn/DetectorDeMutantes/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.utn.DetectorDeMutantes.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
@@ -17,7 +20,15 @@ public class SwaggerConfig {
                             .description("API REST para detectar mutantes y obtener estadísticas")
                             .contact(new Contact()
                                     .name("Paula Bomprezzi")
-                                    .email("paulabomp@gmail.com")));
+                                    .email("paulabomp@gmail.com")))
+                    .servers(List.of(
+                            new Server()
+                                    .url("https://global-detectordemutantes-26-11-1.onrender.com")
+                                    .description("Servidor de Producción (Render)"),
+                            new Server()
+                                    .url("http://localhost:8080")
+                                    .description("Servidor Local (Desarrollo)")
+                    ));
         }
     }
 
